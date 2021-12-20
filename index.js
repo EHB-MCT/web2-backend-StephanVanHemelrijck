fc5c514931241dfe355556bc475bc16fff6c4c2f
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2021-12-20 06:09:30 +0100

    Cors issues

--- index.js
+++ index.js
@@ -32,6 +32,25 @@ const client = new MongoClient(process.env.FINAL_URL);
 // The database to use
 const dbName = process.env.DBNAME;
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1/");
+
+    // Request methods you wish to allow
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+
+    // Request headers you wish to allow
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Root route
 app.get("/", (req, res) => {
     res.status(300).redirect("/info.html");
